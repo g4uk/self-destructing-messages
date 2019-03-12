a54da0a345a812c8ea96d11144992d0ea1cdf87c
Roman Gnatiuk <roman.gnatjuk@gmail.com>
2019-03-13 00:17:34 +0200

    change lib structure

--- app/libs/destruction/methods/number_of_link.rb
+++ app/libs/destruction/methods/number_of_link.rb
@@ -1,6 +1,6 @@
 module Destruction
   module Methods
-    class NumberOfLink < Destruction::Methods::Base
+    class NumberOfLink < Destruction::MethodsBase
       def can_be_destructed
         @item.number_of_link.to_i >= @item.message_destruction.value.to_i
       end
